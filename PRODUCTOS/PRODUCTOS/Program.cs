3b017f349ce2124f8bcef1974560598134913fa8
Ana Laura Oliva <aolivac4@miumg.edu.gt>
2024-03-09 13:06:08 -0600

    Examen parcial

---
+++ PRODUCTOS/PRODUCTOS/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRODUCTOS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido");
+
+            double totalconDescuento = 0;
+            double totalsinDescuento = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+
+                Console.WriteLine($"Ingrese el precio del producto {i}:");
+                double precio = double.Parse(Console.ReadLine());
+                totalconDescuento += precio;
+
+                if (precio > 100)
+
+                {
+                    totalconDescuento += precio;
+                }
+
+                else
+                {
+                    totalsinDescuento += precio;
+                }
+
+
+            }
+
+            Console.WriteLine($"Total con descuento { totalconDescuento}");
+            Console.WriteLine($"Total sin descuento {totalsinDescuento}");
+
+
+
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ SOLUCION1/SOLUCION1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLUCION1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string correo;
+
+            Console.WriteLine("Bienvenido");
+
+            Console.WriteLine("Ingrese su correo electronico de domino gmail.");
+            correo = Console.ReadLine();
+
+            string nombreusuario = correo.Substring(0, correo.IndexOf("@"));
+            string dominioActual = correo.Substring(correo.IndexOf("@") + 1);
+
+            Console.WriteLine("Introduzcal el nuevo dominio");
+            string nuevodominio = Console.ReadLine();
+
+
+            string nuevocorreo = nombreusuario + "@" + nuevodominio;
+
+
+            Console.WriteLine("Su nuevo direccion de correo es " + nuevocorreo);
+
+            Console.ReadKey(true);
+
+        }
+
+       
+    }
+}
